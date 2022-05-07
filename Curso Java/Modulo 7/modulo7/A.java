4889baa9b388935d193da4e5d3acf35a723824c6
Luccas Lima <luccayato@gmail.com>
2022-05-07 14:43:56 -0300

    modulo 7 curso

---
+++ Curso Java/Modulo 7/modulo7/A.java
@@ -0,0 +1,29 @@
+package modulo7;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 60;
+		int nota4 = 80;
+		int media = 0;
+		
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		if (media >=70) {
+			System.out.println("Parabéns você passou de ano: " + media);
+		}
+		if (media >=40 && media <=69) {
+			System.out.println("Está de recuperação: " + media);
+		}
+		
+		else {
+		
+			System.out.println("Aluno foi reprovado: " + media);
+		}
+	}
+
+}
