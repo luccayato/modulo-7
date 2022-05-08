c7e71d59fb3f0dfe0d241924cc8c5849fb76558d
Luccas Lima <luccayato@gmail.com>
2022-05-08 13:35:08 -0300

    att 3

--- Curso Java/Modulo 7/modulo7/A.java
+++ Curso Java/Modulo 7/modulo7/A.java
@@ -5,30 +5,31 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int nota1 = 10;
-		int nota2 = 60;
-		int nota3 = 60;
-		int nota4 = 80;
+		int nota1 = 90;
+		int nota2 = 90;
+		int nota3 = 90;
+		int nota4 = 90;
 		int media = 0;
 		
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
 		
-		/*if (media >=70) {
-			System.out.println("Parabéns você passou de ano: " + media);
-		}
-		if (media >=40 && media <=69) {
-			System.out.println("Está de recuperação: " + media);
-		}
-		
-		else {
-		
-			System.out.println("Aluno foi reprovado: " + media); */
-		
-		String saidaResultado = media >= 70? "Aluno Aprovado" : (media >=40 && media <=69) ? "Aluno em recuperação" : "Aluno Reprovado";
-		
-		System.out.println(saidaResultado);
+		if (media >=50) {
+			if (media >=70) {
+				if (media >=90) {
+					System.out.println("Aluno está aprovado direto - Parabéns");
+				}
+				else 
+				System.out.println("Aluno está aprovado direto");
+			}else {
+				System.out.println("Aluno está em recuperação");
+				
+			}
+		}else {
+			System.out.println("Aluno Reprovado direto");
 		
 	}
+		
+}
 
 }
 
